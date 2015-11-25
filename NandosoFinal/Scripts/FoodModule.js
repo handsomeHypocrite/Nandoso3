6e22f8463d9c0bb876060df34e4943eac13a733c
handsomeHypocrite <kavenchen@hotmail.co.nz>
2015-11-25 19:37:00 +1300

    works

---
+++ NandosoFinal/Scripts/FoodModule.js
@@ -0,0 +1,18 @@
+﻿var FoodModule = (function () {
+    // Return anything that you want to expose outside the closure
+    return {
+        getFood: function (callback) {
+
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://finalnandoso.azurewebsites.net/api/foods",
+                success: function (data) {
+                    console.log(data);
+                    callback(data);
+                }
+            });
+
+        }
+    };
+}());
--- NandosoFinal/index.js
+++ NandosoFinal/index.js
@@ -1,1 +1,35 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", function () {
+    loadFood();
+});
+
+function loadFood() {
+    FoodModule.getFood(setupFoodTable);
+}
+
+function setupFoodTable(FoodList) {
+    var FoodTable = document.getElementById("FoodList");
+    console.log(FoodList);
+
+    for (i = 0; i < FoodList.length; i++) {
+
+        // Create row 
+        var row = document.createElement('tr');
+
+        // Add the columns in the row (td / data cells)
+        var FoodNamecol = document.createElement('td');
+        FoodNamecol.innerHTML = FoodList[i].FoodNme;
+        row.appendChild(FoodNamecol);
+
+        var FoodPricecol = document.createElement('td');
+        FoodPricecol.innerHTML = FoodList[i].FoodPrice;
+        row.appendChild(FoodPricecol);
+
+        var FoodDesccol = document.createElement('td');
+        FoodDesccol.innerHTML = FoodList[i].FoodDesc;
+        row.appendChild(FoodDesccol);
+
+        // Append the row to the end of the table
+        FoodTable.appendChild(row);
+    }
+
+}
