fedc16de9f747bb2dafc6b200d98e84f2629a5ba
handsomeHypocrite <kavenchen@hotmail.co.nz>
2015-11-25 17:21:05 +1300

    setUp1

---
+++ NandosoFinal/Models/Dish.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace NandosoFinal.Models
+{
+    public class Dish
+    {
+        public int ID { get; set; }
+        public string DishName { get; set; }
+        public string DishPrice { get; set; }
+        public string Describtion { get; set; }
+
+        //[JsonIgnore]
+        //public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
---
+++ NandosoFinal/Models/NandosoFinalContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace NandosoFinal.Models
+{
+    public class NandosoFinalContext : DbContext
+    {
+        public class MyConfiguration : DbMigrationsConfiguration<NandosoFinalContext>
+        {
+        }
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public NandosoFinalContext() : base("name=NandosoFinalContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<NandosoFinal.Models.Dish> Dishes { get; set; }
+    
+    }
+}
